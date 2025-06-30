8ea8d50c224db369b6ba69ee29a8bfed1d7f7745
damon <jr.way123@gmail.com>
2025-06-30 14:11:45 +0800

    on OpenWRT device

--- routes/toolset.py
+++ routes/toolset.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template
 from apps.mic_pdf import mic_pdf_bp
-from apps.fba_revise_pdf import fba_revise_pdf_bp
-from apps.product_label_new import product_label_new_bp
 toolset_bp = Blueprint('toolset', __name__)
 
 
@@ -9,15 +7,6 @@ toolset_bp = Blueprint('toolset', __name__)
 def mic_pdf():
     return render_template('toolset/mic_pdf.html')
 
-@toolset_bp.route('/fba-revise-pdf')
-def fba_revise_pdf():
-    return render_template('toolset/fba_revise_pdf.html')
-
-@toolset_bp.route('/product-label-new')
-def product_label_new():
-    return render_template('toolset/product_label_new.html')
 
 # 注册子蓝图
 toolset_bp.register_blueprint(mic_pdf_bp, url_prefix='/mic-pdf')
-toolset_bp.register_blueprint(fba_revise_pdf_bp, url_prefix='/fba-revise-pdf')
-toolset_bp.register_blueprint(product_label_new_bp, url_prefix='/product-label-new')
