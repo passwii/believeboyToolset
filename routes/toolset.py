e688aebb06ebe17f415471254c1184259b7a627d
damon <jr.way123@gmail.com>
2025-06-30 13:45:40 +0800

    feat: add new features and ignore files

--- routes/toolset.py
+++ routes/toolset.py
@@ -1,12 +1,9 @@
 from flask import Blueprint, render_template
 from apps.mic_pdf import mic_pdf_bp
 from apps.fba_revise_pdf import fba_revise_pdf_bp
-
+from apps.product_label_new import product_label_new_bp
 toolset_bp = Blueprint('toolset', __name__)
 
-@toolset_bp.route('/exchange-rate')
-def exchange_rate():
-    return render_template('toolset/exchange_rate.html')
 
 @toolset_bp.route('/mic-pdf')
 def mic_pdf():
@@ -16,6 +13,11 @@ def mic_pdf():
 def fba_revise_pdf():
     return render_template('toolset/fba_revise_pdf.html')
 
+@toolset_bp.route('/product-label-new')
+def product_label_new():
+    return render_template('toolset/product_label_new.html')
+
 # 注册子蓝图
 toolset_bp.register_blueprint(mic_pdf_bp, url_prefix='/mic-pdf')
 toolset_bp.register_blueprint(fba_revise_pdf_bp, url_prefix='/fba-revise-pdf')
+toolset_bp.register_blueprint(product_label_new_bp, url_prefix='/product-label-new')
