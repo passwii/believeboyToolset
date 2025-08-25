0eb26d53d214665436ef101f66335fe7250e1303
damon <jr.way123@gmail.com>
2025-08-25 17:48:02 +0800

    更新时间轴页面内容，添加版本更新日志

--- routes/toolset.py
+++ routes/toolset.py
@@ -1,2 +1,9 @@
 from flask import Blueprint, render_template
+from auth import login_required
+
 toolset_bp = Blueprint('toolset', __name__)
+
+@toolset_bp.route('/update-log')
+@login_required
+def update_log():
+    return render_template('toolset/timeline.html')
